91f139259bd658b05bc25d0dd0032b33abc59da7
Morgan Fogarty <mofo37@gmail.com>
2017-09-18 15:27:35 -0700

    pass state in the form of props from Board to Square.

--- src/index.js
+++ src/index.js
@@ -3,17 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      value: null
-    };
-  }
-
+  
   render() {
     return (
-      <button className="square" onClick={() => this.setState({value: 'X'})}>
-        {this.state.value}
+      <button className="square" onClick={() => this.props.onClick({value: 'X'})}>
+        {this.props.value}
       </button>
     );
   }
@@ -26,9 +20,14 @@ class Board extends React.Component {
       squares: Array(9).fill(null)
     };
   }
-  
+
   renderSquare(i) {
-    return <Square value={i} />;
+    return (
+    <Square
+    value= {this.state.squares[i]}
+    onClick={() => this.handleClick(i)} 
+    />
+    );
   }
 
   render() {
